001bd2a4eeb34127bf3a8f57197ad6546a0a70a8
lunadell <64132424+lunadell@users.noreply.github.com>
2021-10-27 14:59:03 +0200

    Renamed Crosshairs, implemented Crosshair pngs into Weapon Subclasses

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Moorhuhn, but different!
 
 Credits:
-Crosshair6.png as provided by : CC0 - Public Domain Donation by hackcraft.de
+Crosshair pngs as provided by : CC0 - Public Domain Donation by hackcraft.de
--- weapon(Sprite).py
+++ weapon(Sprite).py
@@ -1,31 +1,54 @@
 class IWeapon(Sprite):
+
     
+    def __init__(self, flyweightImages: dict, x: int, y: int, imagename: str, ammo: int):
+        Sprite.__init__(self, flyweightImages: dict, x: int, y: int, imagename: str)
+        self.ammo = ammo 
+
     def update(self):
         # Move crosshair based on mouse position
         pos = pygame.mouse.get_pos()
         self.rect.center = pos
 
-        if self.shoot:
-            # Move crosshair for recoil effect
-            pass
+        ## Kill enemy
+
+        # IF mouseclick, then
+        if event.type == pygame.MOUSEBUTTONDOWN:
+           
+            # CHECK for Ammunition
+            if self.ammo < 0:
+                # Move crosshair for recoil effect
+                
+                # minus one ammo
+                pass
+
+                #IF Crosshair overlap Enemy, then kill
+
+                ''' Mediator'''     
+
+            #IF Ammunition empty, no shot/kill
             
-            ## Kill enemy
-            ''' Mediator'''
-            # Check for Ammunition
 
-            #If Ammo empty, no shot/kill
-            
-
-            #If Hair overlap Enemy, then kill
-    
-
+        
 
 class Weapon1(IWeapon):
-    pass
+    #crosshair1
+    def __init__(self, flyweightImages: dict, x: int, y: int, imagename: str, ammo: int):
+        Sprite.__init__(self, flyweightImages: dict, x: int, y: int, imagename: str)
+        self.ammo = ammo 
+        self.image = self.flyweightImages['crosshair1']
+    
+    
+    
+class Weapon2(IWeapon):
+    #crosshair2
+    def __init__(self, flyweightImages: dict, x: int, y: int, imagename: str, ammo: int):
+        Sprite.__init__(self, flyweightImages: dict, x: int, y: int, imagename: str)
+        self.ammo = ammo 
+        self.image = self.flyweightImages['crosshair2']
     
 
-class Weapon2(IWeapon):
-    pass
+   
 
 
             
